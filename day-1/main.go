57d21dd42d85ce9a7a8de5447c80865ce3ce6b9a
Klaus112 <37701016+Klaus112@users.noreply.github.com>
2025-12-10 23:07:06 +0100

    Solution for Day 1; part 1 and 2

---
+++ day-1/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/klaus112/advent_of_code_2025/files"
+)
+
+type direction struct {
+	value string
+}
+
+func newDirection(value string) direction {
+	return direction{value: value}
+}
+
+var (
+	directionLeft  direction = newDirection("left")
+	directionRight direction = newDirection("right")
+)
+
+type puzzleInput struct {
+	direction direction
+	rotation  uint
+}
+
+func (p puzzleInput) String() string {
+	return fmt.Sprintf("direction:%s; rotation: %d", p.direction.value, p.rotation)
+}
+
+func rotatePart1(p puzzleInput, currentValue uint) uint {
+	rest := p.rotation % 100
+
+	switch p.direction {
+	case directionLeft:
+		if rest > currentValue {
+			// would produce an underflow
+			newVal := 100 + (int(currentValue) - int(rest))
+			return uint(newVal)
+		}
+
+		return currentValue - rest
+	case directionRight:
+		newVal := rest + currentValue
+		if newVal >= 100 {
+			return newVal - 100
+		}
+
+		return newVal
+	}
+
+	panic(fmt.Errorf("should never reach"))
+}
+
+// rotatePart2 returns
+//  1. the amount of times a rotation passed or hit 0.
+//  2. the new value where the rotation landed at.
+func rotatePart2(p puzzleInput, currentValue uint) (uint, uint) {
+	rotationAmount := p.rotation / 100
+	rest := p.rotation % 100
+
+	switch p.direction {
+	case directionLeft:
+		if currentValue == 0 {
+			return rotationAmount, 100 - rest
+		}
+		if rest > currentValue {
+			// would produce an underflow
+			newVal := 100 + (int(currentValue) - int(rest))
+			return rotationAmount + 1, uint(newVal)
+		}
+
+		return rotationAmount, currentValue - rest
+	case directionRight:
+		newVal := rest + currentValue
+		if newVal == 100 {
+			return rotationAmount, newVal - 100
+		}
+		if newVal >= 100 {
+			return rotationAmount + 1, newVal - 100
+		}
+
+		return rotationAmount, newVal
+	}
+
+	panic(fmt.Errorf("should never reach"))
+}
+
+func mustParsePuzzleInput(row string) puzzleInput {
+	direction := directionLeft
+	if row[0] == 'R' {
+		direction = directionRight
+	}
+
+	rotation, err := strconv.ParseUint(row[1:], 10, 64)
+	if err != nil {
+		panic(fmt.Errorf("invalid value in rotation: %s", row[1:]))
+	}
+
+	return puzzleInput{
+		direction: direction,
+		rotation:  uint(rotation),
+	}
+}
+
+//------------------------------------------------------------------
+
+func main() {
+	content := files.ReadIntoSlice(files.DefaultFilePath)
+
+	var (
+		currentRotationValue uint = 50
+		zeroHitsCount        uint
+	)
+	for _, row := range content {
+		puzzleInput := mustParsePuzzleInput(row)
+		currentRotationValue = rotatePart1(puzzleInput, currentRotationValue)
+		if currentRotationValue == 0 {
+			zeroHitsCount++
+		}
+	}
+
+	currentRotationValue = 50
+	var totalZeroHitPassCount uint
+
+	for _, row := range content {
+		puzzleInput := mustParsePuzzleInput(row)
+		zeroHitPassCount, newVal := rotatePart2(puzzleInput, currentRotationValue)
+		currentRotationValue = newVal
+		totalZeroHitPassCount += zeroHitPassCount
+		if currentRotationValue == 0 {
+			totalZeroHitPassCount++
+		}
+	}
+
+	fmt.Printf("Part1: Zero hits count: %d\n", zeroHitsCount)
+	fmt.Printf("Part2: Zero hits + pass count: %d\n", totalZeroHitPassCount)
+}
---
+++ files/files.go
@@ -0,0 +1,33 @@
+package files
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+const (
+	DefaultFilePath     = "input.txt"
+	DefaultTestFilePath = "test_input.txt"
+)
+
+// ReadIntoSlice reads the file at path into a slice of strings (line by line).
+func ReadIntoSlice(path string) []string {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("failed to read file at %s: %w", path, err)
+	}
+	defer f.Close()
+
+	res := make([]string, 0)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		res = append(res, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return res
+}
